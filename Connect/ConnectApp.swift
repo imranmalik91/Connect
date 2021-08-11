bed5d86d49bb0609ff20defd2f3e662e506b4d16
amymalik27 <amymalik27@gmail.com>
2021-08-11 11:35:47 +0530

    Initial Commit

---
+++ Connect/ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectApp.swift
+//  Connect
+//
+//  Created by imran malik on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
